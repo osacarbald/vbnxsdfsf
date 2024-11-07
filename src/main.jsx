bb75257ec73836eabf5ff78362b4dd9dc5946b4e
osacarbald <correctbpuuyesjan@gmail.com>
2024-11-07 04:08:25 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,39 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import Download from './Boss.jsx'
+import ErrorPage from './ErrorPage.jsx';
+
+
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <ErrorPage />,
+  },
+
+  {
+    path: "/download",
+    element: <Download/>,
+    errorElement: <div>Error!</div>,
+
+  
+  },
+
+
+
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+</React.StrictMode>
+);
